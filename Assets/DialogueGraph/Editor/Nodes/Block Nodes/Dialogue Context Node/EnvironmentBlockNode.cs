ac0388d81544429c267ddfc8128b334f28c904bc
AutismoGismo <lucasvanhest2005@gmail.com>
2025-10-05 21:07:16 +0200

    Music queue and multiple sfx

--- Assets/DialogueGraph/Editor/Nodes/Block Nodes/Dialogue Context Node/EnvironmentBlockNode.cs
+++ Assets/DialogueGraph/Editor/Nodes/Block Nodes/Dialogue Context Node/EnvironmentBlockNode.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using TMPro;
 using Unity.GraphToolkit.Editor;
 using UnityEngine;
@@ -9,16 +10,33 @@ using UnityEngine.Audio;
 public class EnvironmentBlockNode : BlockNode
 {
     private const string DelayWithClickName = "delay with click";
+    private const string QueueMusicName = "queue music";
+    private const string PlayMultipleSfxName = "play multiple sfx";
 
     protected override void OnDefineOptions(IOptionDefinitionContext context)
     {
         context.AddOption<bool>(DelayWithClickName);
+        context.AddOption<bool>(QueueMusicName);
+        context.AddOption<bool>(PlayMultipleSfxName);
     }
 
     protected override void OnDefinePorts(IPortDefinitionContext context)
     {
-        context.AddInputPort<AudioResource>("music audio").Build();
-        context.AddInputPort<AudioResource>("dialogue audio").Build();
+        var portTypeOption = GetNodeOptionByName(QueueMusicName);
+        portTypeOption.TryGetValue<bool>(out bool queueMusic);
+
+        portTypeOption = GetNodeOptionByName(PlayMultipleSfxName);
+        portTypeOption.TryGetValue<bool>(out bool playMultipleSfx);
+
+        if (queueMusic)
+            context.AddInputPort<List<AudioResource>>("music audio queue").Build();
+        else
+            context.AddInputPort<AudioResource>("music audio").Build();
+
+        if (playMultipleSfx)
+            context.AddInputPort<List<AudioResource>>("all dialogue audio").Build();
+        else
+            context.AddInputPort<AudioResource>("dialogue audio").Build();
 
         context.AddInputPort<Sprite>("background image").Build();
         context.AddInputPort<bool>("smooth background transition").Build();
--- Assets/DialogueGraph/Editor/Nodes/Context Nodes/DialogueContextNode.cs
+++ Assets/DialogueGraph/Editor/Nodes/Context Nodes/DialogueContextNode.cs
@@ -10,16 +10,26 @@ using UnityEngine.UI;
 public class DialogueContextNode : ContextNode
 {
     private const string EditSettingsName = "edit settings";
+    private const string QueueMusicName = "queue music";
+    private const string PlayMultipleSfxName = "play multiple sfx";
 
     protected override void OnDefineOptions(IOptionDefinitionContext context)
     {
         context.AddOption<bool>(EditSettingsName);
+        context.AddOption<bool>(QueueMusicName);
+        context.AddOption<bool>(PlayMultipleSfxName);
     }
 
     protected override void OnDefinePorts(IPortDefinitionContext context)
     {
         var portTypeOption = GetNodeOptionByName(EditSettingsName);
         portTypeOption.TryGetValue<bool>(out bool editSettings);
+
+        portTypeOption = GetNodeOptionByName(QueueMusicName);
+        portTypeOption.TryGetValue<bool>(out bool queueMusic);
+
+        portTypeOption = GetNodeOptionByName(PlayMultipleSfxName);
+        portTypeOption.TryGetValue<bool>(out bool playMultipleSfx);
 
         context.AddInputPort("in").Build();
 
@@ -30,8 +40,17 @@ public class DialogueContextNode : ContextNode
 
         context.AddInputPort<Sprite>("background image").Build();
         context.AddInputPort<bool>("smooth background transition").Build();
-        context.AddInputPort<AudioResource>("music audio").Build();
-        context.AddInputPort<AudioResource>("dialogue audio").Build();
+
+        if (queueMusic)
+            context.AddInputPort<List<AudioResource>>("music audio queue").Build();
+        else
+            context.AddInputPort<AudioResource>("music audio").Build();
+
+        if (playMultipleSfx)
+            context.AddInputPort<List<AudioResource>>("all dialogue audio").Build();
+        else
+            context.AddInputPort<AudioResource>("dialogue audio").Build();
+
         context.AddInputPort<TMP_FontAsset>("font").Build();
 
         context.AddInputPort<float>("print speed")
